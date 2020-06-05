bb92a032aefbdd8080863c622d0f38f833148c18
rana-abhi777 <abhisekrana777@gmail.com>
2020-06-05 16:46:26 +0530

    day2 commit - datatypes and variables

---
+++ day2-simple-datatype.py
@@ -0,0 +1,9 @@
+# learning datatype and variables in python
+var = 10;
+str1 = "Hello there!"
+
+print(var)
+print(str1)
+
+# how to escape special characters while printing out
+print("hello there, this is a \"quote\"!")
