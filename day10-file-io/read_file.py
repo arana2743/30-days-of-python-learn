2390b911a00af2366734687e18e984cf02230a79
rana-abhi777 <abhisekrana777@gmail.com>
2020-06-21 23:44:47 +0530

    day10 - continued file i/o - learned append method

--- day10-file-io/read_file.py
+++ day10-file-io/read_file.py
@@ -1,3 +1,17 @@
-f = open('test.txt', 'r')
-text = f.read()
-print('Text from file is : ' + str(text))
+def read_file():
+    f = open('test.txt', 'r')
+    text = f.read()
+    print('Text from file is : ' + str(text))
+    f.close()
+
+def append_file():
+    f = open('test.txt', 'a+')
+    text = input('Enter text to feed to the file:\n')
+    f.write(str(text) + '\n')
+
+def main():
+    read_file()
+    append_file()
+
+if __name__ == '__main__':
+    main()
