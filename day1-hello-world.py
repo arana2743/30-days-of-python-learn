374f39dd07252cf7f215360103ae3a8de4c271cf
rana-abhi777 <abhisekrana777@gmail.com>
2020-06-27 23:47:26 +0530

    staging my deleted files

--- day1-hello-world.py
+++
@@ -1,4 +0,0 @@
-import platform
-
-print("Hello world")
-print("We are using python version: {}".format(platform.python_version()))
--- day2-simple-datatype.py
+++
@@ -1,9 +0,0 @@
-# learning datatype and variables in python
-var = 10;
-str1 = "Hello there!"
-
-print(var)
-print(str1)
-
-# how to escape special characters while printing out
-print("hello there, this is a \"quote\"!")
