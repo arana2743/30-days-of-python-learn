8bebb9602237a30064eaf1e929384cc26db37643
rana-abhi777 <abhisekrana777@gmail.com>
2020-08-01 19:28:00 +0530

    list_comprehension - simple example

---
+++ day12-list-comprehension/simple_example.py
@@ -0,0 +1,7 @@
+# simple example of list comprehension
+
+# spliting a string into single words array
+letter_arrays = [letter for letter in 'HelloWorld']
+print(letter_arrays)
+
+# [expression for item in list]
